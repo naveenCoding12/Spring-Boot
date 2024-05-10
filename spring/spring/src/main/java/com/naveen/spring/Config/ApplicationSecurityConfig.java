60bf559f048b3db4d111870a391a7d6e35510514
Naveenkumar <naveenkumar.cenimeni@achalasolutions.com>
2024-05-10 11:56:55 +0530

    "spring security with data base  and bcrypt password "

--- spring/spring/src/main/java/com/naveen/spring/Config/ApplicationSecurityConfig.java
+++ spring/spring/src/main/java/com/naveen/spring/Config/ApplicationSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +23,9 @@ public class ApplicationSecurityConfig {
   public AuthenticationProvider authenticationProvider() {
     DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
     provider.setUserDetailsService(userDetailsService);
+    // no password encoder here
+//    provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+    // if we want password encoder
     provider.setPasswordEncoder(passwordEncoder());
     return provider;
   }
